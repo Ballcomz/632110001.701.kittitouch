9b85d2e404cfb422fb121465936382f268e6dc3d
ballcomz <ballcomz@gmail.com>
2021-01-12 01:44:37 +0700

    3 hello

--- test/hello.java
+++ test/hello.java
@@ -2,6 +2,6 @@ package test;
 
 public class hello {
     public static void main(String[] args) {
-    System.out.println("hello from aj ball at CAMT");
+    System.out.println("hello from aj ball at CAMT, CMU.");
 }
 }
